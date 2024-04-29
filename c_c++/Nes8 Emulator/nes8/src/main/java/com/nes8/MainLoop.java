760616b5b280440a88723a4c862f1c47e75426ba
harish.kumar <harish.kumar@zohocorp.com>
2024-04-30 00:34:44 +0530

    NES Emulator

---
+++ c_c++/Nes8 Emulator/nes8/src/main/java/com/nes8/MainLoop.java
@@ -0,0 +1,13 @@
+package com.nes8;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainLoop 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
