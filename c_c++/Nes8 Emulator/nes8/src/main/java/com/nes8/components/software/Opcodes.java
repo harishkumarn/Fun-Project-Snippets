e32482b230591ac36dff30ebe3d9112e8540189b
harish.kumar <harish.kumar@zohocorp.com>
2024-05-01 16:17:28 +0530

    6502 AND inst implemented

--- c_c++/Nes8 Emulator/nes8/src/main/java/com/nes8/components/software/Opcodes.java
+++ c_c++/Nes8 Emulator/nes8/src/main/java/com/nes8/components/software/Opcodes.java
@@ -22,8 +22,13 @@ public class Opcodes {
         cpu.updateFlag(Flag.V, ((~(a ^ o) & (a ^ (a + o + c))) & 0x0080)  > 0 );
     } 
 
+    private void updateANDFlags(int val){
+        cpu.updateFlag(Flag.Z, (val & 0xFF) == 0);
+        cpu.updateFlag(Flag.N, (val & 0x80) > 0 );
+    }
+
     private void initOpcodes(){
-        // ADD
+        // ADC
         opcodes.put(0x69, new Opcode((byte)2,(byte)2, cpu){
             @Override
             public byte execute(){
@@ -115,6 +120,104 @@ public class Opcodes {
                 updateADCFlags(cpu.accumulator,cpu.bus.cpuRead(address ) , cpu.getFlag(Flag.C));
                 cpu.accumulator = (byte)(temp & 0xFF);
                 System.out.println("ADC ("+ Integer.toHexString(operand) + ") ,Y" );
+                return (byte)cycle;
+            }
+        });
+
+        //--------------------------------------
+        //AND
+
+        opcodes.put(0x69, new Opcode((byte)2,(byte)2, cpu){
+            @Override
+            public byte execute(){
+                byte operand = cpu.bus.getByteCode(cpu.programCounter++);
+                int temp = cpu.accumulator & operand ;
+                updateANDFlags(temp);
+                cpu.accumulator = (byte)(temp & 0xFF);
+                System.out.println("AND #"+ Integer.toHexString(operand));
+                return (byte)cycle;
+            }
+        });
+        opcodes.put(0x65, new Opcode((byte)2,(byte)3, cpu){
+            // Zero page
+            @Override
+            public byte execute(){
+                byte operand = cpu.bus.getByteCode(cpu.programCounter++);
+                int temp = cpu.accumulator & cpu.bus.cpuRead(operand);
+                updateANDFlags(temp);
+                cpu.accumulator = (byte)(temp & 0xFF);
+                System.out.println("AND "+ Integer.toHexString(operand));
+                return (byte)cycle;
+            }
+        });
+        opcodes.put(0x75,new Opcode((byte)2,(byte)4, cpu){
+            @Override
+            public byte execute(){
+                byte operand = cpu.bus.getByteCode(cpu.programCounter++);
+                int temp = cpu.accumulator & cpu.bus.cpuRead(operand + cpu.indexX);
+                updateANDFlags(temp);
+                cpu.accumulator = (byte)(temp & 0xFF);
+                System.out.println("AND "+ Integer.toHexString(operand) + " ,X");
+                return (byte)cycle;
+            }
+        });
+        opcodes.put(0x6D,new Opcode((byte)3,(byte)4, cpu){
+            @Override
+            public byte execute(){
+                byte operand1 = cpu.bus.getByteCode(cpu.programCounter++);
+                byte operand2 = cpu.bus.getByteCode(cpu.programCounter++);
+                int temp = cpu.accumulator & cpu.bus.cpuRead( (operand1 << 8 ) + operand2) ;
+                updateANDFlags(temp);
+                cpu.accumulator = (byte)(temp & 0xFF);
+                System.out.println("AND "+ Integer.toHexString((operand1 << 8 ) + operand2) );
+                return (byte)cycle;
+            }
+        });
+        opcodes.put(0x7D,new Opcode((byte)3,(byte)4, cpu){
+            @Override
+            public byte execute(){
+                byte operand1 = cpu.bus.getByteCode(cpu.programCounter++);
+                byte operand2 = cpu.bus.getByteCode(cpu.programCounter++);
+                int temp = cpu.accumulator & cpu.bus.cpuRead( (operand1 << 8 ) + operand2 + cpu.indexX) ;
+                updateANDFlags(temp);
+                cpu.accumulator = (byte)(temp & 0xFF);
+                System.out.println("AND "+ Integer.toHexString((operand1 << 8 ) + operand2) + " ,X" );
+                return (byte)cycle;
+            }
+        });
+        opcodes.put(0x79,new Opcode((byte)3,(byte)4, cpu){
+            @Override
+            public byte execute(){
+                byte operand1 = cpu.bus.getByteCode(cpu.programCounter++);
+                byte operand2 = cpu.bus.getByteCode(cpu.programCounter++);
+                int temp = cpu.accumulator & cpu.bus.cpuRead( (operand1 << 8 ) + operand2 + cpu.indexY) ;
+                updateANDFlags(temp);
+                cpu.accumulator = (byte)(temp & 0xFF);
+                System.out.println("AND "+ Integer.toHexString((operand1 << 8 ) + operand2) + " ,Y" );
+                return (byte)cycle;
+            }
+        });
+        opcodes.put(0x61,new Opcode((byte)2,(byte)6, cpu){
+            @Override
+            public byte execute(){
+                byte operand = cpu.bus.getByteCode(cpu.programCounter++);
+                int address = (cpu.bus.cpuRead(operand + 1) << 8 )  + cpu.bus.cpuRead(operand );
+                int temp = cpu.accumulator & cpu.bus.cpuRead(address ) ;
+                updateANDFlags(temp);
+                cpu.accumulator = (byte)(temp & 0xFF);
+                System.out.println("AND ("+ Integer.toHexString(operand) + " ,X)" );
+                return (byte)cycle;
+            }
+        });
+        opcodes.put(0x71,new Opcode((byte)2,(byte)5, cpu){
+            @Override
+            public byte execute(){
+                byte operand = cpu.bus.getByteCode(cpu.programCounter++);
+                int address = (cpu.bus.cpuRead(operand + 1) << 8 )  + cpu.bus.cpuRead(operand ) +  cpu.indexY;
+                int temp = cpu.accumulator & cpu.bus.cpuRead(address ) ;
+                updateANDFlags(temp);
+                cpu.accumulator = (byte)(temp & 0xFF);
+                System.out.println("AND ("+ Integer.toHexString(operand) + ") ,Y" );
                 return (byte)cycle;
             }
         });
